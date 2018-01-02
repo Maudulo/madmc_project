80ba4421b5ef756c793f6f4dea0b1b0a3867e13d
Maud Béguey <ma.be92@hotmail.fr>
2018-01-02 15:58:53 +0100

    v2 dynamic prgramming
    not working

--- MADMC_project.py
+++ MADMC_project.py
@@ -95,6 +95,11 @@ def pareto_dominants(l):
 
 
 def arg_pareto_dominants(l):
+	"""
+	@params l : la liste des objets dans laquelle trouver les Pareto-dominants
+	@return p_dominants la liste des Pareto-dominants
+			arg_list la liste des indices des Pareto-dominants dans l
+	"""
 	p_dominants = pareto_dominants(l)
 	arg_list = []
 	for x in p_dominants:
@@ -107,8 +112,6 @@ def arg_pareto_dominants(l):
 def construct_arg_list(l, arg_list):
 	list_pareto = []
 	for element in arg_list:
-		print("coucou", len(l))
-		print("kbdns", element)
 		list_pareto.append(l[element])
 	return list_pareto
 
@@ -123,7 +126,7 @@ def dynamic_programming(list_obj, size_max):
 
 		Les fonctions de récurrence sont : 
 		dp_list[0,0] = list_obj[0]
-		dp_list[1,l] = p_l U dp_list[1,l-1]
+		dp_list[1,l] = Pareto(p_l U dp_list[1,l-1])
 		dp_list[k,l] = Pareto((dp_list[k-1, l-1] + p_l) U dp_list[k, l-1])
 
 	"""
@@ -131,17 +134,23 @@ def dynamic_programming(list_obj, size_max):
 		print("Il ne peut pas y avoir de solution avec ", size_max, " élements dans un ensemble de ", len(list_obj), " éléments")
 		sys.exit()
 
-	dp_list = np.empty((len(list_obj), size_max), dtype = object)
-	obj_list = np.empty((len(list_obj), size_max), dtype = object)
+	dp_list = np.empty((len(list_obj), size_max), dtype = object) # liste des P-opt (somme des objets)
+	dp_obj = np.empty((len(list_obj), size_max), dtype = object) # liste des objets composant les P-opt
 
 	dp_list[0][0] = np.array([list_obj[0]])
-	obj_list[0][0] = np.array([list_obj[0]])
+	dp_obj[0][0] = dp_list[0][0]
+	print("initialisation : ", dp_obj[0][0])
 
 	# pour chaque objet de la liste
 	for l in range(1, len(list_obj)):
 
-		dp_list[l][0], arg_pareto = arg_pareto_dominants(np.concatenate((np.array([list_obj[l]]), dp_list[l - 1][0])))
-		obj_list[l][0] = construct_arg_list(np.concatenate((np.array([list_obj[l]]), obj_list[l - 1][0])), arg_pareto)
+		dp_list[l][0], index_p_opt = arg_pareto_dominants(np.concatenate((np.array([list_obj[l]]), dp_list[l - 1][0])))
+
+		temp_list = []
+		for i in index_p_opt:
+			temp_list.append(list_obj[i])
+		dp_obj[l][0] = temp_list
+		print("étape suivante : ", dp_obj[l][0])
 		
 		# pour chaque taille d'objet
 		for k in range(1, size_max):
@@ -149,16 +158,39 @@ def dynamic_programming(list_obj, size_max):
 			if k == l + 1:
 				break
 
-			sum_objs = np.add(dp_list[l - 1][k - 1], list_obj[l])
-			objs = np.add(obj_list[l - 1][k - 1], list_obj[l])
+			objs = np.add(dp_list[l - 1][k - 1], list_obj[l])
 
 			if k != l: # si k == l, alors k>l-1 et la case du dessus n'est pas remplie
-				sum_objs = np.concatenate((sum_objs, dp_list[l - 1][k]))
+				objs = np.concatenate((objs, dp_list[l - 1][k]))
 
-			dp_list[l][k], arg_pareto = arg_pareto_dominants(np.array(sum_objs))
-			obj_list[l][k] = construct_arg_list(np.array(objs), arg_pareto)
+			dp_list[l][k], index_p_opt = arg_pareto_dominants(np.array(objs))
 
-	return arg_pareto_dominants(dp_list[len(list_obj)-1][size_max-1])
+			list_bidon = np.array([])
+
+			print(type(np.array(dp_obj[l - 1][k - 1])))
+			print(type(np.array([list_obj[l]])))
+
+			print(np.array(dp_obj[l - 1][k - 1]))
+			print(np.array([list_obj[l]]))
+
+			print("somme : ", np.concatenate((np.array(dp_obj[l - 1][k - 1]), np.array([list_obj[l]])), axis = 0))
+
+
+			print(type(np.add(dp_obj[l - 1][k - 1], list_obj[l])))
+			print(type(dp_obj[l - 1][k]))
+			list_bidon = np.concatenate(np.add(dp_obj[l - 1][k - 1], list_obj[l]), dp_obj[l - 1][k])
+			print(list_bidon, "\n")
+
+
+
+
+			temp_list = []
+			for i in index_p_opt:
+				temp_list.append(list_bidon[i])
+			dp_obj[l][k] = temp_list
+			print(dp_obj[l][k])
+
+	return dp_list[len(list_obj)-1][size_max-1], dp_obj[len(list_obj)-1][size_max-1]
 
 def minimax_value(l, alpha_min, alpha_max):
 	"""
