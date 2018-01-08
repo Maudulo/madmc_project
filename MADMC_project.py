a451dbaecaceb3bd685718e3002d8431c4613610
Ethernam <p_fmonville@hotmail.fr>
2018-01-08 16:31:59 +0100

    final

--- MADMC_project.py
+++ MADMC_project.py
@@ -48,9 +48,9 @@ def naive_pareto_dominants_2(l):
 	"""
 	pareto_dominants = []
 	go_on = True
-	to_delete=[]
 	new_l = l
 	while(go_on):
+		to_delete = []
 		pareto_dominant = True
 		for j in range(len(pareto_dominants)):
 			if is_pareto_dominant(pareto_dominants[j], new_l[0]):
@@ -93,7 +93,7 @@ def pareto_dominants(l):
 
 	return pareto_dominants
 
-def pareto_dominants_2(l):
+def pareto_dominants_2(l, ph1, ph2):
 	"""
 	find all vectors that are not pareto dominated in l
 	by sorting l in a lexicographic order then 
@@ -136,7 +136,7 @@ def construct_arg_list(l, arg_list):
 	return list_pareto
 
 
-def dynamic_programming_2(L, K, I_dominance = False):
+def dynamic_programming_2(L, K, I_dominance = False, display_all_array = False, alpha_min = 0, alpha_max = 1):
 	if(K > len(L)):
 		print("Il ne peut pas y avoir de solution avec ", K, " élements dans un ensemble de ", len(L), " éléments")
 		sys.exit()
@@ -152,7 +152,7 @@ def dynamic_programming_2(L, K, I_dominance = False):
 	tab[0,0] = [Element(L[0],[L[0]])]
 
 	for l in range(1,len(L)):
-		tab[l,0] = get_Dominators(np.concatenate((copy.deepcopy(tab[l-1,0]), [Element(L[l],[L[l]])])))
+		tab[l,0] = get_Dominators(np.concatenate((copy.deepcopy(tab[l-1,0]), [Element(L[l],[L[l]])])), alpha_min, alpha_max)
 
 	for l in range(1, len(L)):
 		for k in range(1, K):
@@ -164,58 +164,62 @@ def dynamic_programming_2(L, K, I_dominance = False):
 				o.elements = np.concatenate((o.elements,[L[l]]))
 			if k != l: # si k == l, alors k>l-1 et la case du dessus n'est pas remplie
 				objs = np.concatenate((objs, copy.deepcopy(tab[l-1,k])))
-			tab[l,k] = get_Dominators(objs)
+			tab[l,k] = get_Dominators(objs, alpha_min, alpha_max)
 
 	# return (tab[len(L)-1,K-1].sum, tab[len(L)-1,K-1].elements)
-	return tab[len(L)-1,K-1]
+	if display_all_array:
+		return tab
+	else:
+		return tab[len(L)-1,K-1]
+
 
 
 def dynamic_programming(list_obj, size_max, i_dominant = False, alpha_min = 0, alpha_max = 1):
-    """
-        @params list_obj la liste des objets dont on veut trouver un sous ensemble
-                size_max la taille du sous ensemble d'objets souhaité
-        @return la liste des paretos dominants
+	"""
+		@params list_obj la liste des objets dont on veut trouver un sous ensemble
+				size_max la taille du sous ensemble d'objets souhaité
+		@return la liste des paretos dominants
 
-        Cette fonction trouve l'ensemble des paretos optimaux par la programmation dymnamique
+		Cette fonction trouve l'ensemble des paretos optimaux par la programmation dymnamique
 
-        Les fonctions de récurrence sont : 
-        dp_list[0,0] = list_obj[0]
-        dp_list[1,l] = Pareto(p_l U dp_list[1,l-1])
-        dp_list[k,l] = Pareto((dp_list[k-1, l-1] + p_l) U dp_list[k, l-1])
+		Les fonctions de récurrence sont : 
+		dp_list[0,0] = list_obj[0]
+		dp_list[1,l] = Pareto(p_l U dp_list[1,l-1])
+		dp_list[k,l] = Pareto((dp_list[k-1, l-1] + p_l) U dp_list[k, l-1])
 
-    """
-    if(size_max > len(list_obj)):
-        print("Il ne peut pas y avoir de solution avec ", size_max, " élements dans un ensemble de ", len(list_obj), " éléments")
-        sys.exit()
+	"""
+	if(size_max > len(list_obj)):
+		print("Il ne peut pas y avoir de solution avec ", size_max, " élements dans un ensemble de ", len(list_obj), " éléments")
+		sys.exit()
 
-    dp_list = np.empty((len(list_obj), size_max), dtype = object) # liste des P-opt (somme des objets)
+	dp_list = np.empty((len(list_obj), size_max), dtype = object) # liste des P-opt (somme des objets)
 
-    dp_list[0][0] = np.array([list_obj[0]])
+	dp_list[0][0] = np.array([list_obj[0]])
 
-    # pour chaque objet de la liste
-    for l in range(1, len(list_obj)):
-        if not I_dominant:
-            dp_list[l][0], index_p_opt = arg_pareto_dominants(np.concatenate((np.array([list_obj[l]]), dp_list[l - 1][0])))
-        else:
-            dp_list[l][k] = I_dominant(np.concatenate((np.array([list_obj[l]]), dp_list[l - 1][0])), alpha_min, alpha_max)
+	# pour chaque objet de la liste
+	for l in range(1, len(list_obj)):
+		if not I_dominant:
+			dp_list[l][0], index_p_opt = arg_pareto_dominants(np.concatenate((np.array([list_obj[l]]), dp_list[l - 1][0])))
+		else:
+			dp_list[l][k] = I_dominant(np.concatenate((np.array([list_obj[l]]), dp_list[l - 1][0])), alpha_min, alpha_max)
 
-        # pour chaque taille d'objet
-        for k in range(1, size_max):
+		# pour chaque taille d'objet
+		for k in range(1, size_max):
 
-            if k == l + 1:
-                break
+			if k == l + 1:
+				break
 
-            objs = np.add(dp_list[l - 1][k - 1], list_obj[l])
+			objs = np.add(dp_list[l - 1][k - 1], list_obj[l])
 
-            if k != l: # si k == l, alors k>l-1 et la case du dessus n'est pas remplie
-                objs = np.concatenate((objs, dp_list[l - 1][k]))
+			if k != l: # si k == l, alors k>l-1 et la case du dessus n'est pas remplie
+				objs = np.concatenate((objs, dp_list[l - 1][k]))
 
-            if not I_dominant:
-                dp_list[l][k], index_p_opt = arg_pareto_dominants(np.array(objs))
-            else:
-                dp_list[l][k] = I_dominant(np.array(objs), alpha_min, alpha_max)
+			if not I_dominant:
+				dp_list[l][k], index_p_opt = arg_pareto_dominants(np.array(objs))
+			else:
+				dp_list[l][k] = I_dominant(np.array(objs), alpha_min, alpha_max)
 
-    return dp_list[len(list_obj)-1][size_max-1], dp_obj[len(list_obj)-1][size_max-1]
+	return dp_list[len(list_obj)-1][size_max-1], dp_obj[len(list_obj)-1][size_max-1]
 
 
 def minimax_value(l, alpha_min, alpha_max):
@@ -255,71 +259,96 @@ def minimax_value_2(l, alpha_min = 0, alpha_max = 1):
 
 def I_dominant(l, alpha_min = 0, alpha_max = 1):
 
-    I_dominator = []
-    to_delete = []
-    new_l = copy.deepcopy(l)
+	I_dominator = []
+	new_l = copy.deepcopy(l)
 
-    while(len(new_l) != 0):
-        element = l[0]
-        i_dominant = True
+	while(len(new_l) != 0):
+		to_delete = []
+		element = l[0]
+		i_dominant = True
 
-        # pour tous les autres éléments
-        for i in range(1,len(l)):
-            element2 = l[i]
+		# pour tous les autres éléments
+		for i in range(1,len(l)):
+			element2 = l[i]
 
-            if dominate(element, element2, alpha_min, alpha_max):
-                to_delete.append(i)
+			if dominate(element, element2, alpha_min, alpha_max):
+				to_delete.append(i)
 
-            elif dominate(element2, element, alpha_min, alpha_max):
-                i_dominant = False
-                break
+			elif dominate(element2, element, alpha_min, alpha_max):
+				i_dominant = False
+				break
 
-        if(i_dominant):
-            I_dominator.append(element)
-        to_delete.append(0)
+		if(i_dominant):
+			I_dominator.append(element)
+		to_delete.append(0)
 
-        new_l = [i for (j, i) in enumerate(new_l) if (j not in to_delete)]
+		new_l = [i for (j, i) in enumerate(new_l) if (j not in to_delete)]
 
-    return I_dominator
+	return I_dominator
 
 def I_dominant_2(l, alpha_min = 0, alpha_max = 1):
 
-    I_dominator = []
-    to_delete = []
-    new_l = copy.deepcopy(l)
+	not_I_dominated = []
+	new_l = copy.deepcopy(l)
 
-    while(len(new_l) != 0):
-        element = l[0]
-        i_dominant = True
+	while(len(new_l) != 0):
+		# init the list where goes all elements that are not I_dominated
+		to_delete = []
+		# process the first element
+		element = new_l[0]
+		# it will be removed at the end and possibly added to the not_I_dominated list
+		to_delete.append(0)
 
-        # pour tous les autres éléments
-        for i in range(1,len(l)):
-            element2 = l[i]
+		i_dominant = True
 
-            if dominate(element.sum, element2.sum, alpha_min, alpha_max):
-                to_delete.append(i)
+		# pour tous les autres éléments
+		for i in range(1,len(new_l)):
+			element2 = new_l[i]
 
-            elif dominate(element2.sum, element.sum, alpha_min, alpha_max):
-                i_dominant = False
-                break
+			if dominate2(element.sum, element2.sum, alpha_min, alpha_max):
+				to_delete.append(i)
 
-        if(i_dominant):
-            I_dominator.append(copy.deepcopy(element))
-        to_delete.append(0)
+			elif dominate2(element2.sum, element.sum, alpha_min, alpha_max):
+				i_dominant = False
+				break
 
-        new_l = [i for (j, i) in enumerate(new_l) if (j not in to_delete)]
+		if(i_dominant):
+			not_I_dominated.append(copy.deepcopy(element))
 
-    return I_dominator
+		new_l = [i for (j, i) in enumerate(new_l) if (j not in to_delete)]
+
+	return not_I_dominated
 
 
 
 
 def fi(element, alpha):
+	"""
+	return the result of a*y1 + (1-a)*y2
+	"""
+
 	return element[0] * alpha + element[1] * (1 - alpha)
 
 def dominate(e1, e2, alpha_min, alpha_max):
-	return fi(e1, alpha_min) <= fi(e2, alpha_min) and fi(e1, alpha_max) <= fi(e2, alpha_max) and (fi(e1, alpha_min) < fi(e2, alpha_min) or fi(e1, alpha_max) < fi(e2, alpha_max))
+	"""
+	return True if e1 I-dominate e2 false ef e1 not I-dominate e2
+	e2 can still I-dominate e1 or be incomparable to it
+	you need to test the other to know if they are incomparable
+	"""
 
+	# test if <= for alpha max and alpha min
+	less_or_equal = (fi(e1, alpha_min) <= fi(e2, alpha_min)) and (fi(e1, alpha_max) <= fi(e2, alpha_max))
+	# test if < for alpha max or alpha min
+	strictly_less = (fi(e1,alpha_min) < fi(e2,alpha_min)) or (fi(e1,alpha_max) < fi(e2,alpha_max))
+
+	return less_or_equal and strictly_less
+	
+	# return fi(e1, alpha_min) <= fi(e2, alpha_min) and fi(e1, alpha_max) <= fi(e2, alpha_max) and (fi(e1, alpha_min) < fi(e2, alpha_min) or fi(e1, alpha_max) < fi(e2, alpha_max))
+
+def dominate2(e1,e2,alpha_min, alpha_max):
+	cst1 = (e1[0]-e2[0])
+	cst2 = (e2[1]-e1[1])
+	return ((alpha_min*cst1) <= ((1-alpha_min)*cst2)) and ((alpha_max*cst1) <= ((1-alpha_max)* cst2)) and (((alpha_min*cst1) < ((1-alpha_min)*cst2)) or ((alpha_max*cst1) < ((1-alpha_max)* cst2)))
 
 class Element:
 
--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from test_pareto_func import *
 from MADMC_project import *
+import numpy as np
+import matplotlib.pyplot as plt
 
 
+def printAllArray(arr):
+	for line in arr:
+		for k in line:
+			print("[", end="")
+			if k != None:
+				for i,x in enumerate(k):
+					print(":", x.sum,":",end="")
+			else:
+				print("None", end="")
+			print("], ",end="")
+		print()
 
+np.set_printoptions(formatter={'float': '{: 0.2f}'.format})
 # vector_generated = gaussian_vector_generator(10, 50)
 
 # print("valeurs générées : ", vector_generated)
@@ -19,32 +33,33 @@ from MADMC_project import *
 
 # plot_pareto_compare_functions(naive_pareto_dominants, pareto_dominants, nmin = 20, nmax = 500, step = 25, n = 50)
 
-l = gaussian_vector_generator(6, 10)
-k = 4
-print("\nliste : \n", l)
+# l = gaussian_vector_generator(5, 10)
+# k = 3
+# print("\nliste : \n", l)
 
-dp1 = dynamic_programming_2(l, k, I_dominance = False)
-# print("nombre d'éléments dans chaque case du tableau de prog dyn ",[[len(x) if x else 0 for x in y] for y in dp1])
-print("\nil y a ", len(dp1), " solutions")
-i = 0
-for x in dp1:
-	i+=1
-	print("\nsolution : ", i)
-	print("p opt de taille ", k," ", x.sum)
-	print("somme composée de ", x.elements)
+# dp1 = dynamic_programming_2(l, k, I_dominance = False)
+# # print("nombre d'éléments dans chaque case du tableau de prog dyn ",[[len(x) if x else 0 for x in y] for y in dp1])
+# print("\nil y a ", len(dp1), " solutions")
 
-dp2 = dynamic_programming_2(l, k, I_dominance = True)
-# print("nombre d'éléments dans chaque case du tableau de prog dyn ",[[len(x) if x else 0 for x in y] for y in dp2])
-print("\nil y a ", len(dp2), " solutions")
-i = 0
-for x in dp2:
-	i+=1
-	print("\nsolution : ", i)
-	print("p opt de taille ", k," ", x.sum)
-	print("somme composée de ", x.elements)
+# for i,x in enumerate(dp1):
+# 	print("\nsolution : ", i)
+# 	print("p opt de taille ", k," ", x.sum)
+# 	print("somme composée de ", x.elements)
 
-minmax = minimax_value_2(dp1)
-print("minimax avec pareto : ", minmax.sum, " composé de : " , minmax.elements)
+# printAllArray(dynamic_programming_2(l, k, I_dominance = True, display_all_array = True))
+# dp2 = dynamic_programming_2(l, k, I_dominance = True)
+# #print("nombre d'éléments dans chaque case du tableau de prog dyn ",[[len(x) if x else 0 for x in y] for y in dp2])
+# print("\nil y a ", len(dp2), " solutions")
+# for i,x in enumerate(dp2):
+# 	print("\nsolution : ", i)
+# 	print("p opt de taille ", k," ", x.sum)
+# 	print("somme composée de ", x.elements)
+
+
+# # minmax = minimax_value_2(dp1)
+# # print("minimax avec pareto : ", minmax.sum, " composé de : " , minmax.elements)
+# minmax = minimax_value_2(dp2)
+# print("minimax avec I dominance : ", minmax.sum, " composé de : " , minmax.elements)
 
 
 
@@ -56,3 +71,34 @@ print("minimax avec pareto : ", minmax.sum, " composé de : " , minmax.elements)
 # print("meilleur minimax avec alpha_min = ", alpha_min, " alpha_max = ", alpha_max, "\n", minimax_dynamic_programming(l, k, alpha_min = 0, alpha_max = 1))
 
 # test_all_pareto_functions([naive_pareto_dominants, pareto_dominants])
+# 
+def tests(n = 50):
+	t1,t2 = 0,[0 for _ in range(20)]
+	for i in range(n):
+		print("test :",i)
+		l = gaussian_vector_generator(50, 1000)
+		k = 10
+		start = time.time()
+		dp1 = dynamic_programming_2(l, k, I_dominance = False)
+		t1 += time.time() - start
+
+		for i,a in enumerate(np.arange(0.025,0.525,0.025)):
+			start = time.time()
+			dp2 = dynamic_programming_2(l, k, I_dominance = True, alpha_min=0.5-a, alpha_max= 0.5+a)
+			t2[i] += time.time() - start
+
+	return t1/n,np.array(t2)/n
+
+
+def display_result_tests(results):
+	t1,t2 = results
+	x = np.arange(0.025,0.525,0.025)
+	t1 = [t1 for _ in range(len(t2))]
+	plt.figure(1)
+	plt.plot(x,t1)
+	plt.plot(x,t2)
+	plt.show()
+	print(t1)
+	print(t2)
+
+display_result_tests(tests(50))
